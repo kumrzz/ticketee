8c293593cce46ca8ff2fb4e328e574f33d16d34b
kumrzz <kumrzz@gmail.com>
2015-05-15 15:09:19 +0100

    Only admins can create or delete projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
   root "application#index"
+
+  resources :projects, only: [:new, :create, :destroy]
   end
 
   devise_for :users
@@ -17,7 +19,7 @@ root 'projects#index'
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-resources :projects do
+resources :projects, only: [:index, :show, :edit, :update] do
  resources :tickets
 end
 
