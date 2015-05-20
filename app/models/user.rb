f6e739a2bdc52878e6a8e72f976739e9637a42b2
kumrzz <kumrzz@gmail.com>
2015-05-20 13:18:00 +0100

    Admins can edit project roles of existing users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :roles
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   scope :excluding_archived, lambda { where(archived_at: nil) }
   
-  def to_s
-   "#{email} (#{admin? ? "Admin" : "User"})"
-  end
+ def to_s
+  "#{email} (#{admin? ? "Admin" : "User"})"
+ end
   
-  def archive
-   self.update(archived_at: Time.now)
-  end
+ def archive
+  self.update(archived_at: Time.now)
+ end
 
 	def active_for_authentication?
 	 super && archived_at.nil?
@@ -20,6 +21,10 @@ class User < ActiveRecord::Base
 
 	def inactive_message
  	 archived_at.nil? ? super : :archived
+	end
+
+	def role_on(project)
+ 	roles.find_by(project_id: project).try(:name)
 	end
 
 end
